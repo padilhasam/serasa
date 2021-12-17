652f5839c4e80feae70823cba62c685896409a0e
padilhasam <jeferson.padilha86@gmail.com>
2021-12-17 15:50:44 -0300

    Site Serasa

---
+++ js/card.js
@@ -0,0 +1,101 @@
+/* Script Modal AGRs - Página Unidades*/
+/* CARD 1 */
+/* Botão 1 */
+// select the open-btn button
+let openBtn = document.getElementById('open-btn');
+// select the modal-background
+let modalBackground = document.getElementById('modal-background');
+// select the close-btn
+let closeBtn = document.getElementById('close-btn');
+// shows the modal when the user clicks open-btn
+openBtn.addEventListener('click', function() {
+  modalBackground.style.display = 'block';
+});
+// hides the modal when the user clicks close-btn
+closeBtn.addEventListener('click', function() {
+  modalBackground.style.display = 'none';
+});
+// hides the modal when the user clicks outside the modal
+window.addEventListener('click', function(event) {
+  // check if the event happened on the modal-background
+  if (event.target === modalBackground) {
+    // hides the modal
+    modalBackground.style.display = 'none';
+  }
+});
+/*  Finaliza Botão 1 */
+
+/* Botão 2 */
+// select the open-btn button
+let openBtn2 = document.getElementById('open-btn2');
+// select the modal-background
+let modalBackground2 = document.getElementById('modal-background2');
+// select the close-btn
+let closeBtn2 = document.getElementById('close-btn2');
+// shows the modal when the user clicks open-btn
+openBtn2.addEventListener('click', function() {
+  modalBackground2.style.display = 'block';
+});
+// hides the modal when the user clicks close-btn
+closeBtn2.addEventListener('click', function() {
+  modalBackground2.style.display = 'none';
+});
+// hides the modal when the user clicks outside the modal
+window.addEventListener('click', function(event) {
+  // check if the event happened on the modal-background
+  if (event.target === modalBackground2) {
+    // hides the modal
+    modalBackground2.style.display = 'none';
+  }
+});
+/*  Finaliza Botão 2 */
+
+/* Botão 3 */
+// select the open-btn button
+let openBtn3 = document.getElementById('open-btn3');
+// select the modal-background
+let modalBackground3 = document.getElementById('modal-background3');
+// select the close-btn
+let closeBtn3 = document.getElementById('close-btn3');
+// shows the modal when the user clicks open-btn
+openBtn3.addEventListener('click', function() {
+  modalBackground3.style.display = 'block';
+});
+// hides the modal when the user clicks close-btn
+closeBtn3.addEventListener('click', function() {
+  modalBackground3.style.display = 'none';
+});
+// hides the modal when the user clicks outside the modal
+window.addEventListener('click', function(event) {
+  // check if the event happened on the modal-background
+  if (event.target === modalBackground3) {
+    // hides the modal
+    modalBackground3.style.display = 'none';
+  }
+});
+/*  Finaliza Botão 3 */
+
+/* Botão 4 */
+// select the open-btn button
+let openBtn4 = document.getElementById('open-btn4');
+// select the modal-background
+let modalBackground4 = document.getElementById('modal-background4');
+// select the close-btn
+let closeBtn4 = document.getElementById('close-btn4');
+// shows the modal when the user clicks open-btn
+openBtn4.addEventListener('click', function() {
+  modalBackground4.style.display = 'block';
+});
+// hides the modal when the user clicks close-btn
+closeBtn4.addEventListener('click', function() {
+  modalBackground4.style.display = 'none';
+});
+// hides the modal when the user clicks outside the modal
+window.addEventListener('click', function(event) {
+  // check if the event happened on the modal-background
+  if (event.target === modalBackground4) {
+    // hides the modal
+    modalBackground4.style.display = 'none';
+  }
+});
+/*  Finaliza Botão 4 */
---
+++ menu.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-br"  dir="ltr">
+  <head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+  <!-- ===============================================-->
+  <!--    Document Title-->
+  <!-- ===============================================-->
+  <title>Certificado Digital Serasa</title>
+
+
+  <!-- ===============================================-->
+  <!--    Favicons-->
+  <!-- ===============================================-->
+  <link rel="apple-touch-icon" sizes="180x180" href="assets/img/favicons/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicons/favicon32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicons/favicon16x16.png">
+  <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicons/favicon.png">
+  <link rel="manifest" href="assets/img/favicons/manifest.json">
+  <meta name="msapplication-TileImage" content="assets/img/favicons/mstile-150x150.png">
+  <meta name="theme-color" content="#ffffff">
+
+
+  <!-- ===============================================-->
+  <!--    Stylesheets-->
+  <!-- ===============================================-->
+  <link href="assets/css/theme.css" rel="stylesheet" />
+  </head>
+  <body>
+    <main class="main" id="top">
+      <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" data-navbar-on-scroll="data-navbar-on-scroll">
+        <div class="container"><a class="navbar-brand" href="index.html"><img class="img-fluid" src="assets/img/icons/logo1.png" alt="Serasa Experian" /></a>
+          <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto ms-lg-4 ms-xl-7 border-bottom border-lg-bottom-0 pt-2 pt-lg-0">
+              <li class="nav-item"><a class="nav-link fw-bold active" aria-current="page" href="index.php">Início</a></li>
+              <li class="nav-item"><a class="nav-link fw-medium" href="index.php#sobre">Sobre</a></li>
+              <li class="nav-item"><a class="nav-link fw-medium" href="index.php#beneficios">Benefícios</a></li>
+              <li class="nav-item"><a class="nav-link fw-medium" href="index.php#depoimentos">Depoimentos</a></li>
+            </ul>
+            <form class="d-flex py-3 py-lg-0"><a class="btn btn-light rounded-pill shadow fw-bold" href="https://serasa.certificadodigital.com.br/parceiro/id-br-digital/" role="button" target="_blank">Adquira seu Certificado<svg class="bi bi-arrow-right" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#9C69E2" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"></path></svg></a></form>
+          </div>
+        </div>
+      </nav>
+  </body>
+</html>
